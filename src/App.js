ea62b5f6a59346173841082c47df6988c6dc7a29
yokesh-code <yokeshsre@gmail.com>
2023-06-20 12:22:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    // Fetch product data from the backend
+    axios.get('http://localhost:3000/scrape')
+      .then(response => setProduct(response.data))
+      .catch(error => console.error('Error fetching product data', error));
+  }, []);
+
+  const handleCreateProduct = () => {
+    const newProduct = {
+      title: 'New Product',
+      rating: 4.5,
+      price: 99.99,
+      finalPrice: 79.99
+    };
+
+    axios.post('http://localhost:3000/scrape', newProduct)
+      .then(response => {
+        console.log('Product created:', response.data);
+        setProduct(response.data);
+      })
+      .catch(error => console.error('Error creating product', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product Information</h1>
+      {product ? (
+        <div>
+          <h2>{product.title}</h2>
+          <p>Rating: {product.rating}</p>
+          <p>Price: {product.price}</p>
+          <p>Final Price: {product.finalPrice}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={handleCreateProduct}>Create Product</button>
     </div>
   );
 }
